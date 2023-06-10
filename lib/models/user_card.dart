3f951c06b2f008c2678e7b206e392e358de0eeb0
Ana Marija Pavičić <ana.marija.pavicic@talentlyft.com>
2023-06-10 17:01:34 +0200

    Update the code

--- lib/models/user_card.dart
+++ lib/models/user_card.dart
@@ -3,12 +3,14 @@ class UserCardData {
   final String cardId;
   final String themeId;
   final String userId;
-  final int value;
+  final bool completed; // completed
+  final DateTime dataCompleted; // completed
 
   UserCardData({
+    required this.dataCompleted,
     required this.uid,
     required this.cardId,
-    required this.value,
+    required this.completed,
     required this.userId,
     required this.themeId,
   });
